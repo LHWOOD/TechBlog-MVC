ba2cbc1dce0de7f7105d8f2b46368d4f6f4cf199
LHWOOD <hutsonwood@gmail.com>
2021-05-29 15:50:16 -0600

    initial file setup

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
+
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Create the Handlebars.js engine object with custom helper functions
+//creates our handlebars object, also passing any methods...
 const hbs = exphbs.create({ helpers });
 
-// Inform Express.js which template engine we're using
+const sess = {
+  secret: "Super secret secret",
+  cookie: {}, //could pass a max age that this cookie exists
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+//sessions allows us to access and store data across requests, cookies on our server
+app.use(session(sess));
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
