5f081a7fce43e69bca3d5f43aa394e855423a9bb
LHWOOD <hutsonwood@gmail.com>
2021-05-30 16:10:11 -0600

    worked on api routes and finished and testes router.get in user.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,3 +7,5 @@ const commentRoutes = require("./commentRoutes.js");
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use("/comments", commentRoutes);
+
+module.exports = router;
