2edb264f5620948f12eea59b0be4b8a7248be957
LHWOOD <hutsonwood@gmail.com>
2021-05-30 11:36:06 -0600

    established the associations between the models in the index.js file

--- models/index.js
+++ models/index.js
@@ -1,3 +1,30 @@
 const User = require("./User");
+const Comment = require("./Comment");
+const Post = require("./Post");
 
-module.exports = { User };
+//establishes the assocaitions between the models
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Comment, Post };
