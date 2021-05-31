f87f64828f8e5d37f58ba000acd867233df0e206
LHWOOD <hutsonwood@gmail.com>
2021-05-30 21:31:48 -0600

    created and test the get and post route for the commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,4 +2,30 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+//get comments
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//create comments //not tested
+router.post("/", withAuth, (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    })
+      .then((commentData) => res.json(commentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
 module.exports = router;
